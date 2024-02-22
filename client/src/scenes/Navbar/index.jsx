66f64e1e23efc54de06865ead4b143e109a62d71
Sameer <meersj@gmail.com>
2024-02-22 21:56:02 +0545

    added name display in Navbar

--- client/src/scenes/Navbar/index.jsx
+++ client/src/scenes/Navbar/index.jsx
@@ -29,11 +29,7 @@ const Navbar = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // const user = useSelector((state) => state.user);
-  const user = {
-    firstName: 'Joe',
-    lastName: 'Mama',
-  };
+  const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
 
   const theme = useTheme();
