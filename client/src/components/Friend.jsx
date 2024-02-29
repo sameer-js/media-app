74f2ac6650dc1084a2c061a951cbe195003dd08b
Sameer <meersj@gmail.com>
2024-02-29 12:40:20 +0545

    fixed adding self

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -19,7 +19,11 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
 
-  const isFriend = friends.find((friend) => friend._id === friendId);
+  const isSelf = friendId === _id;
+  const isFriend =
+    friends.length > 0
+      ? friends.find((friend) => friend._id === friendId)
+      : false;
 
   const patchFriend = async () => {
     const response = await fetch(
@@ -64,16 +68,18 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: '0.6rem' }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+      {!isSelf && (
+        <IconButton
+          onClick={() => patchFriend()}
+          sx={{ backgroundColor: primaryLight, p: '0.6rem' }}
+        >
+          {isFriend ? (
+            <PersonRemoveOutlined sx={{ color: primaryDark }} />
+          ) : (
+            <PersonAddOutlined sx={{ color: primaryDark }} />
+          )}
+        </IconButton>
+      )}
     </FlexBetween>
   );
 };
